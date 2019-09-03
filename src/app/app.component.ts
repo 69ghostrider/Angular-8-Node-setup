948483e19dc394b7939de8a07d8efbcc149a0afb
joshua_salema <joshuasalema@gmail.com>
2019-09-03 22:36:23 +0530

    Updating code to enhance communication and add basic validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NgForm } from '@angular/forms';
 import { DataService } from './../../services/data.service';
+import {NgbCalendar,NgbDatepickerConfig,NgbDateStruct,NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';;
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -7,12 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestSetup';
-   someArray :string[]= ['9', '2', '5'];
+  data : any = {};
+  model: NgbDateStruct;
+  minDate :any;
+  ckecklist = false;
+  someArray :string[]= ['9', '2', '5'];
+  checkBoxArray: { id: number, name: string }[] = [ { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' } ] 
+   constructor(private modalService: NgbModal,private dataservice : DataService,private calendar: NgbCalendar,config: NgbDatepickerConfig){
+    config.minDate = {year: 1900, month: 1, day: 1};
+    config.maxDate = {year: 2099, month: 12, day: 31};
    
-   constructor(private dataservice : DataService){
-    //  dataservice.getdata().subscribe(result => {
-    //    console.log("Reached Component")
-    //  })
+    config.outsideDays = 'hidden';
+    this.checkBoxArray.forEach( (element) => {
+    element.selected = false;
+     });
+    console.log(this.checkBoxArray)
    }
    onSave(){
      localStorage.setItem('key', 'value');
@@ -20,6 +40,30 @@ export class AppComponent {
        console.log("Reached Component")
      })
    }
- 
+    onSubmit(f : NgForm){
+      console.log(f)
+      console.log(this.data)
+      this.dataservice.storage = this.data;
+      console.log(this.dataservice.storage)
+      console.log(this.checkBoxArray)
+    }
+ closeResult: string;
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
 
